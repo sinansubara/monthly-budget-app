4f10be91adea789c8ceaaea777c3d101cbaa447b
Sinan Šubara <sinan@superthread.com>
2025-05-12 02:46:56 +0200

    feat: add ButtonCustom component with customizable text and disabled state

---
+++ src/components/ButtonCustom.vue
@@ -0,0 +1,54 @@
+<template>
+  <button
+    class="button-custom-wrap"
+    :disabled="props.disabled"
+    @click="handleClick"
+  >
+    <slot>
+      <span
+        v-if="props.buttonText"
+        class="button-text"
+        >{{ props.buttonText }}</span
+      >
+    </slot>
+  </button>
+</template>
+
+<script setup>
+const props = defineProps({
+  buttonText: {
+    type: String,
+    default: '',
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+});
+const emit = defineEmits(['click']);
+
+const handleClick = () => {
+  if (props.disabled) return;
+  emit('click');
+};
+</script>
+
+<style lang="scss">
+.button-custom-wrap {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: $secondary-color;
+  padding: 16px;
+  border-radius: 4px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+
+  .button-text {
+    font-size: 14px;
+    line-height: 20px;
+    color: $dark-text;
+  }
+}
+</style>
