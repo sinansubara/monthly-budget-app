aca2f08ab74d40c9b5d00ba8353ffcb91a53033c
Sinan Šubara <sinan@superthread.com>
2025-05-13 00:07:15 +0200

    feat: add InfoCard component for displaying information with a title and content slot

---
+++ src/components/widgets/InfoCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="info-card">
+    <div class="card-header">
+      <span class="card-header-title">{{ title }}</span>
+    </div>
+    <div
+      v-if="$slots.content"
+      class="card-body"
+    >
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    default: '',
+  },
+});
+</script>
+
+<style lang="scss">
+.info-card {
+  display: flex;
+  flex-direction: column;
+  background-color: $light-background;
+  padding: 16px 26px 26px;
+  gap: 22px;
+
+  .card-header {
+    display: flex;
+    justify-content: center;
+    padding: 0px 8px 8px;
+
+    // add border bottom only if card-header is not last element
+    &:not(:last-child) {
+      border-bottom: 1px dotted $border-color;
+    }
+
+    .card-header-title {
+      font-size: 30px;
+      line-height: 36px;
+      color: $dark-text;
+    }
+  }
+}
+</style>
