53327e8b2167a848f24de4783840bfc6bb3ce3d9
Sinan Šubara <sinan@superthread.com>
2025-05-12 11:12:35 +0200

    feat: add localStorage utility functions for managing user data

---
+++ src/utilities/localStorageUtils.js
@@ -0,0 +1,46 @@
+const browserStorageGetItem = (key) => {
+  const item = window?.localStorage.getItem(key);
+  if (item === null || item === 'undefined') {
+    return null;
+  }
+  return JSON.parse(item);
+};
+const browserStorageSetItem = (key, value) => {
+  const valueToStore =
+    typeof value === 'string' ? value : JSON.stringify(value);
+  window?.localStorage.setItem(key, valueToStore);
+};
+const browserStorageRemoveItem = (key) => {
+  window?.localStorage.removeItem(key);
+};
+
+const getCurrentUserIdKey = () => {
+  return 'current_user_id';
+};
+
+const getCurrentUserId = () => {
+  const currentUserKey = getCurrentUserIdKey();
+  const currentUserId = browserStorageGetItem(currentUserKey);
+  return currentUserId ?? null;
+};
+
+const getUserKey = (userId) => {
+  return `user_${userId}`;
+};
+
+const getUserData = (userId) => {
+  // implement logic for current user also here
+  const id = userId ?? getCurrentUserId();
+  if (!id) return null;
+  const userKey = getUserKey(id);
+  const userData = browserStorageGetItem(userKey);
+  return userData;
+};
+
+export {
+  browserStorageGetItem,
+  browserStorageSetItem,
+  browserStorageRemoveItem,
+  getCurrentUserId,
+  getUserData,
+};
