ac7c7e06572b00de9804314359eb95654dc99b70
Sinan Šubara <sinan@superthread.com>
2025-05-12 19:14:17 +0200

    feat: add vite-svg-loader for SVG support and update package dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,29 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import svgLoader from 'vite-svg-loader';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    svgLoader({
+      svgoConfig: {
+        multipass: true,
+        plugins: [
+          {
+            name: 'preset-default',
+            params: {
+              overrides: {
+                // viewBox is required to resize SVGs with CSS.
+                removeViewBox: false,
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
