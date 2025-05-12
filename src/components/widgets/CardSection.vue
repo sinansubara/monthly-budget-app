0aed7f1e1e5a08749783696ae400eef0320c179a
Sinan Šubara <sinan@superthread.com>
2025-05-13 01:28:16 +0200

    feat: create CardSection component with title and slot support

---
+++ src/components/widgets/CardSection.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card-section box-shadow">
+    <span class="card-section-title">{{ title }}</span>
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    default: '',
+  },
+});
+</script>
+
+<style lang="scss">
+.card-section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: $box-background;
+  gap: 4px;
+  border-radius: 4px;
+  padding: 10px 2px;
+  width: 100%;
+
+  .card-section-title {
+    font-size: 12px;
+    line-height: 16px;
+    color: $black-text;
+    font-weight: 700;
+    text-transform: uppercase;
+  }
+}
+</style>
