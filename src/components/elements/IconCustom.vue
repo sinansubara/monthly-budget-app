5bf453ee7ae1f40ba42749b99e5dac1eb9fb85ea
Sinan Šubara <sinan@superthread.com>
2025-05-12 19:14:33 +0200

    feat: create IconCustom component for dynamic SVG icon loading

---
+++ src/components/elements/IconCustom.vue
@@ -0,0 +1,19 @@
+<template>
+  <component :is="icon" />
+</template>
+
+<script setup>
+import { defineAsyncComponent } from 'vue';
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+const icon = defineAsyncComponent(
+  // () => import(`../assets/svg/${props.name}.svg`),
+  () => import(`@/assets/icons/${props.name}.svg`),
+);
+</script>
