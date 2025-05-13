a753f4343aff2455f78aa59984a5c3a8ffc663a6
Sinan Šubara <sinan@superthread.com>
2025-05-13 16:39:46 +0200

    feat: add FixExpenses component with expense selection functionality

---
+++ src/components/widgets/FixExpenses.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="fix-expenses">
+    <div
+      v-for="(expense, index) in fixExpenses"
+      :key="index"
+      class="fix-expense-item"
+    >
+      <ImageCircle
+        class="image-placeholder"
+        :image-name="expense.logo"
+      />
+      <span class="fix-expense-name">{{ expense.name }}</span>
+      <ButtonCustom
+        button-text="select"
+        button-variation="small-button"
+        @click="handleSelectExpense(expense)"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import fixedExpenses from '@/data/fixedExpenses.json';
+import ButtonCustom from '@/components/elements/ButtonCustom.vue';
+import ImageCircle from '@/components/elements/ImageCircle.vue';
+
+import { computed } from 'vue';
+
+const fixExpenses = computed(() => {
+  return fixedExpenses.map((expense) => ({
+    name: expense.name,
+    amount: expense.amount,
+    logo: expense.logo,
+  }));
+});
+
+const handleSelectExpense = (expense) => {
+  console.log(`Selected expense: ${expense.name} - ${expense.amount}`);
+  // Here you can add logic to handle the selected expense
+};
+</script>
+
+<style lang="scss">
+.fix-expenses {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 21px;
+
+  .fix-expense-item {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+
+    .fix-expense-name {
+      font-size: 12px;
+      line-height: 16px;
+      color: $black-text;
+      letter-spacing: 9%;
+      text-transform: uppercase;
+    }
+
+    .button-custom-wrap {
+      margin-left: auto;
+    }
+  }
+}
+</style>
