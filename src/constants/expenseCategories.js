ea5de64bd1edc34b6c0ce44614cf8206c56a306a
Sinan Šubara <sinan@superthread.com>
2025-05-13 16:47:54 +0200

    feat: add ExpenseCategory constant for expense categorization

---
+++ src/constants/expenseCategories.js
@@ -0,0 +1,10 @@
+export const ExpenseCategory = {
+  OCIO: 'ocio',
+  FOOD: 'food',
+  SALUD: 'salud',
+  SAVINGS: 'savings',
+  DEBTS: 'debts',
+  SUBSCRIPTION: 'subscription',
+  VARIOUS: 'various',
+  RENT: 'rent',
+};
