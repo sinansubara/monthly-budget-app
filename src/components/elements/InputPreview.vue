ec9aa003175431019f6bdc8bf59e0d67ab83bfa9
Sinan Šubara <sinan@superthread.com>
2025-05-14 22:22:01 +0200

    feat: add InputPreview component for customizable input display

---
+++ src/components/elements/InputPreview.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="input-custom input-preview">
+    <slot>
+      <div class="placeholder">{{ placeholder }}</div>
+    </slot>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  placeholder: {
+    type: String,
+    default: '',
+  },
+});
+</script>
