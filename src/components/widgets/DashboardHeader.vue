b9ae4dc184adb5535570e8712ca868014c85d637
Sinan Šubara <sinan@superthread.com>
2025-05-14 03:30:18 +0200

    feat: enhance styling and layout of DashboardHeader component for improved responsiveness and user experience

--- src/components/widgets/DashboardHeader.vue
+++ src/components/widgets/DashboardHeader.vue
@@ -70,7 +70,7 @@ const handleNewExpense = () => {
 
       .header-subtitle {
         font-size: 12px;
-        line-height: 16px;
+        line-height: 20px;
         color: $subtitle-text;
       }
 
@@ -98,15 +98,80 @@ const handleNewExpense = () => {
   }
 
   @media only screen and (min-width: 600px) {
+    .header-welcome-text {
+      font-size: 18px;
+      line-height: 28px;
+    }
     .dashboard-header-wrap {
-      .new-expense-button {
-        display: flex;
-        padding: 10px 32px;
-        margin-left: auto;
+      .money-off-icon {
+        width: 69px;
+        height: 69px;
+      }
+      .header-title-wrap {
+        .header-subtitle {
+          font-size: 16px;
+          line-height: 28px;
+        }
 
-        .button-text {
-          font-size: 18px;
-          line-height: 22px;
+        .main-header-title {
+          font-size: 36px;
+        }
+      }
+      .header-right-wrap {
+        display: flex;
+        margin-left: auto;
+        gap: 23px;
+
+        .new-expense-button {
+          padding: 10px 32px;
+
+          .button-text {
+            font-size: 18px;
+            line-height: 22px;
+          }
+        }
+      }
+    }
+  }
+
+  @media only screen and (min-width: 992px) {
+    .header-welcome-text {
+      display: none;
+    }
+    .dashboard-header-wrap {
+      .money-off-icon {
+        width: 103px;
+        height: 103px;
+      }
+      .header-title-wrap {
+        .header-subtitle {
+          font-size: 20px;
+          line-height: 36px;
+          font-weight: 600;
+        }
+
+        .main-header-title {
+          font-size: 54px;
+          font-weight: 800;
+        }
+      }
+      .header-right-wrap {
+        .right-user-info {
+          display: flex;
+          align-items: center;
+          gap: 6px;
+
+          .user-icon {
+            width: 38px;
+            height: 38px;
+            color: $light-color-base;
+          }
+
+          .user-name {
+            font-size: 16px;
+            line-height: 24px;
+            color: $white-text;
+          }
         }
       }
     }
