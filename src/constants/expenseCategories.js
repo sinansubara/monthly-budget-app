54c5d762ca1fa3b1853a2065a34c81c41762f71f
Sinan Šubara <sinan@superthread.com>
2025-05-14 21:04:51 +0200

    feat: add ExpenseCategoryIcons and ListExpenseCategories for improved expense categorization

--- src/constants/expenseCategories.js
+++ src/constants/expenseCategories.js
@@ -19,3 +19,57 @@ export const ExpenseCategoryNames = {
   [ExpenseCategory.VARIOUS]: 'Various',
   [ExpenseCategory.RENT]: 'Rent',
 };
+
+export const ExpenseCategoryIcons = {
+  [ExpenseCategory.OCIO]: 'ocio',
+  [ExpenseCategory.FOOD]: 'food',
+  [ExpenseCategory.SALUD]: 'salud',
+  [ExpenseCategory.SAVINGS]: 'savings',
+  [ExpenseCategory.DEBTS]: 'debts',
+  [ExpenseCategory.SUBSCRIPTION]: 'subscription',
+  [ExpenseCategory.VARIOUS]: 'various',
+  [ExpenseCategory.RENT]: 'rent',
+};
+
+export const ListExpenseCategories = [
+  {
+    id: ExpenseCategory.OCIO,
+    name: ExpenseCategoryNames[ExpenseCategory.OCIO],
+    icon: ExpenseCategoryIcons[ExpenseCategory.OCIO],
+  },
+  {
+    id: ExpenseCategory.FOOD,
+    name: ExpenseCategoryNames[ExpenseCategory.FOOD],
+    icon: ExpenseCategoryIcons[ExpenseCategory.FOOD],
+  },
+  {
+    id: ExpenseCategory.SALUD,
+    name: ExpenseCategoryNames[ExpenseCategory.SALUD],
+    icon: ExpenseCategoryIcons[ExpenseCategory.SALUD],
+  },
+  {
+    id: ExpenseCategory.SAVINGS,
+    name: ExpenseCategoryNames[ExpenseCategory.SAVINGS],
+    icon: ExpenseCategoryIcons[ExpenseCategory.SAVINGS],
+  },
+  {
+    id: ExpenseCategory.DEBTS,
+    name: ExpenseCategoryNames[ExpenseCategory.DEBTS],
+    icon: ExpenseCategoryIcons[ExpenseCategory.DEBTS],
+  },
+  {
+    id: ExpenseCategory.SUBSCRIPTION,
+    name: ExpenseCategoryNames[ExpenseCategory.SUBSCRIPTION],
+    icon: ExpenseCategoryIcons[ExpenseCategory.SUBSCRIPTION],
+  },
+  {
+    id: ExpenseCategory.VARIOUS,
+    name: ExpenseCategoryNames[ExpenseCategory.VARIOUS],
+    icon: ExpenseCategoryIcons[ExpenseCategory.VARIOUS],
+  },
+  {
+    id: ExpenseCategory.RENT,
+    name: ExpenseCategoryNames[ExpenseCategory.RENT],
+    icon: ExpenseCategoryIcons[ExpenseCategory.RENT],
+  },
+];
