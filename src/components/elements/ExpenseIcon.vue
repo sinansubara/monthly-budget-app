51eb70e8f787dd8eacfb71cb7bfc4aed4e80e0f9
Sinan Šubara <sinan@superthread.com>
2025-05-14 21:20:10 +0200

    feat: add ExpenseIcon component for displaying expense icons

---
+++ src/components/elements/ExpenseIcon.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="expense-icon">
+    <ImageCircle
+      v-if="isImageLogo"
+      :image-name="iconName"
+    />
+    <IconCustom
+      v-else
+      :name="iconName"
+      class="expense-icon"
+    />
+  </div>
+</template>
+
+<script setup>
+import ImageCircle from '@/components/elements/ImageCircle.vue';
+import IconCustom from '@/components/elements/IconCustom.vue';
+
+defineProps({
+  iconName: {
+    type: String,
+    default: 'savings',
+  },
+  isImageLogo: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
