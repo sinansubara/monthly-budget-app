8732f810320e3f04873bd6e3c20183d0448b6565
Sinan Šubara <sinan@superthread.com>
2025-05-15 02:28:03 +0200

    fix: update import paths for styles and App component in main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
-import './styles/app.scss';
-import App from './App.vue';
+import '@/styles/app.scss';
+import App from '@/App.vue';
 import { createPinia } from 'pinia';
 import router from '@/router';
 
